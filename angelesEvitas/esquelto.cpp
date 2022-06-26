46d9abe8b7ca85ad141624dc299b84d08dbf6892
c1t1gz <joaquin15poggi@gmail.com>
2022-06-26 00:50:57 +0000

    PORFIN TERMINE EL CODIGO y reporte mejorado

--- angelesEvitas/esquelto.cpp
+++ angelesEvitas/esquelto.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <unistd.h>
 #include <sys/wait.h>
@@ -7,136 +6,272 @@
 #include <math.h>
 #include <sys/mman.h>  
 #include <string.h>  
-#include <atomic>
 #include <tuple>
 #include <cassert>
+#include <atomic>
+#include <fstream>
 
 using namespace std;
 
-
 void* share_mem(int size);
-// Los detalles de esta funcion NO son relevantes para el TP!! Pero los aclaro 
-//por las dudas
-// Esta funcion pide size * sizeof(atomic<float>) bytes dinámicamente
-// Este espacio permite reservar suficiendte memoria para un ARREGLO de size 
-//enteros atomicos. Nos devuelve un puntero (un tipo similar a las referencias)
-// al comienzo de este espacio contiguo de memoria.
-// Nota: sizeof es una funcion que nos dice el tamaño de un tipo, en este caso
-// atomic float.
+tuple<int, float, string, float> spawnChildren(int intentosNacimiento, float birthProb, float rageProb, string unitType);
+void angeles(int dias, int n, float p2, atomic<float> *qtyAngeles, atomic<float> *angelesPoder);
+void evitas(int dias, int m, float p1, float p3, atomic<float> *qtyEvas, atomic<float> *evitasPoder);
+float Powers(string my_type);
 
-tuple<string, int> spawn_children(int max_days,int day, atomic<float> *newborns,
- 								  float p, float p2, int max_children,
- 								  string my_type);
-// Esta funcion simula el proceso de nacimiento de evitas y angeles que se lleva
-// a cabo en el dia day. Tener en cuenta que esta función será ejecutada por 
-// todos los evitas/angeles que puedan reproducirse en el dia day.
-// Ademas del dia de creación, recibe como parametro el arreglo newborns 
-// (expresado como puntero, pero eso no les molesta, pueden usar la sintaxis de
-// arreglos como siempre) que debe ser actualizado con los nuevos evitas/angeles
-// que resulten creados en este dia.
-// La funcion debe crear los procesos necesarios y devolver una tupla cuya
-// primera componente indique el tipo creado (evita, angel) y el caso especial 
-// de "parent".
 
 int main(int argc, char* argv[])
 {
-	// Pensar el main como proceso "simulacion" que puede controlar todo.
-	// La responsabilidad es la de recopilar información de la simualación.
+	int max_days = atoi(argv[1]);
+	int m = atoi(argv[2]); 
+	int n = atoi(argv[3]); 
+	float p1 = stof(argv[4]);
+	float p2 = stof(argv[5]);
+	float p3 = stof(argv[6]);
+    float margen = stof(argv[7]);
 
-	// Utilizamos los parámetros que recibe main para obtener los valores de la 
-	// simulacion (ver orden en pdf)
-	int max_days = atoi(argv[1]); 
-	int M = atoi(argv[2]); 
-	int N = atoi(argv[3]); 
-	float ps[3];
-	ps[0] = stof(argv[4]);
-	ps[1] = stof(argv[5]);
-	ps[2] = stof(argv[6]);
-	float eps = stof(argv[7]);
+    atomic<float> *qtyEvas = (atomic<float> *) share_mem(max_days + 1);
+	atomic<float> *evitasPoder = (atomic<float> *) share_mem(max_days + 1);
+	atomic<float> *angelesPoder = (atomic<float> *) share_mem(max_days + 1);
+    atomic<float> *qtyAngeles = (atomic<float> *) share_mem(max_days + 1);
 
- 	// Vamos a almacenar la cantidad de evitas/angeles nuevos al termino de cada
- 	// dia en un arreglo de floats y el poder acumulado de cada especia por día.
- 	// Podemos pensarlo como un arreglo de 2 filas x max_days columnas.
- 	// Es probable que necesiten que este arreglo sea compartido por todos los 
- 	// procesos.
- 	// Notar que en realidad estamos usando un float "atomico". Esto nos da 
- 	// propiedades interesantes para evitar algunos problemas en caso de que
- 	// ocurran escrituras concurrentes sobre el mismo espacio de memoria.
- 	// Pueden asumir que funciona como cualquier otro float. 
-	
- 	int size = max_days;
-	atomic<float> *evitas = (atomic<float> *) share_mem(size);
-	atomic<float> *angeles = (atomic<float> *) share_mem(size);
+    cout << "El nacimiento de los angeles fue: " << endl;
+    int pid = fork();
+    if (pid == 0){
+        
+		angeles(max_days, n, p2, qtyAngeles, angelesPoder);   
+		exit(0);
+    }
+    else{
 
-	// Por las dudas, fijamos a 0 todos los valores
-	for(int i = 0;i < 2*size; i++)evitas[i] = 0;
-	for(int i = 0;i < 2*size; i++)angeles[i] = 0;
+        wait(NULL);
+    
+    }
 
-		// Inicializamos el conteo para para el dia 0
-	evitas[0] = 1;
-	angeles[0] = 1;	
-  	
-  	// Atributos de cada especie. Sugerencia, setear cada variable según tipo.
-  	int pid;
-  	atomic<float> *newborns;
-  	float max_children,p, p_beserk;
-  	string my_type = "";
+    cout << "El nacimiento de los evitas fue: " << endl;
+    pid = fork();
+    if (pid == 0){
+        
+		evitas(max_days, m, p1, p3, qtyEvas, evitasPoder);
+        exit(0);
+    }
+    else{
 
-  	// Crear a Adam y Lilith 
-	/*COMPLETAR*/
-	pid = fork();
-       	if (pid != 0 && pid > 0 )
-	{
-		int lilith = fork();
-		if (lilith == 0)
-		{
-			cout << "Soy lilith";
-		}
-		wait(NULL);
+        wait(NULL);
+    
+    }
+
+    for (int dias = 1; dias <= max_days; dias++){
+        
+        // Aca ya seteamos el poder
+		angelesPoder[dias] = angelesPoder[dias] + angelesPoder[dias - 1];
+        evitasPoder[dias] = evitasPoder[dias] + evitasPoder[dias - 1];
+    
 	}
-	if (pid == 0)
-	{
-		my_type = ""
-	}
-	wait(NULL);
 
-	// El control de la simulación debería esperar la finalización para comenzar
-	// a computar la información necesaria.
-		
-	// En este punto, solo Adam y Lilith deberian llegar
-  	// Variables que representan los dias y la cantidad de hijos de un día.
- 	int day, nchildren;
- 	
- 	// Corremos las simulaciones de nacimiento
-	for(day = 1; day < max_days; day++)
-	{
-		// Simulamos un dia y obtenemos los resultados
-		tuple<string,int> result = spawn_children(max_days,day,newborns, p,
-												  p_beserk, max_children,my_type);
-		string type = get<0>(result);
-		nchildren = get<1>(result);
-	
-		if (type == "parent")
-		{	
+    bool diaEncontrado = false;
+    
+	for (int dias = 1; dias <= max_days; dias++){
+        if (angelesPoder[dias] - evitasPoder[dias] < margen){
+            
+            // Aca ya muestra el dia que sobrevivimos en el caso de hacerlo 
 			
-		}
-
-		// Si llego acá, es porque nací en este día.
-		// Tengo que avanzar al próximo día de la simulación	
-		
-		assert(type != "parent");
-		continue;
+            diaEncontrado = true;
+            
+            cout << "Se encontro un dia que coincide con las simulaciones y el margen. El dia que sobrevivimos es el dia numero: " << dias << endl;
+            
+            break;
+    
+	    }
+    }
+    if (diaEncontrado == false){
+        
+		cout << "Nos re morimos, no hay dias donde nos salvamos" << endl;
+    
 	}
 
-	printf("[%d][dia %d] %s hoja, engendrado de %d \n",getpid(),
-			day,my_type.c_str(), getppid());
+    // Se crea el archivo
+    std::ofstream pepe("resultado.csv");
+    
+    // Escribo en el archivo
+    pepe << max_days << " " << m << " " << n << " " << p1 << " " << p2 << " " << p3 << " " << margen << "\n";
+    
+	for (int dias = 0; dias <= max_days; dias++){
+    
+	    pepe << "Dia: " << dias << ", Evitas: " << qtyEvas[dias] << ", Angeles: " << qtyAngeles[dias] << "\n";
+    
+	}
+    
+    // Cerrio el archivo
+    pepe.close();
+
+    exit(0);
 
 }
 
+void angeles(int dias, int n, float p2, atomic<float> *qtyAngeles, atomic<float> *angelesPoder){
+
+    float poderHijo = 0;
+    int ppid = getpid();
+    string type = "Angeles";
+    float poderAdam = Powers(type);
+    qtyAngeles[0] = 1;
+    angelesPoder[0] = poderAdam;
+
+    for (int diaActual = 1; diaActual <= dias; diaActual++){
+        if(type == "Angeles"){
+            
+			tuple<int, float, string, float> simulacion = spawnChildren(n, p2, -1, type);
+            
+			if ("parent" == get<2>(simulacion)){
+                
+				type = get<2>(simulacion);
+                qtyAngeles[diaActual] = qtyAngeles[diaActual] + get<0>(simulacion);
+                angelesPoder[diaActual] = angelesPoder[diaActual] + get<1>(simulacion);
+                
+				if (ppid == getpid()){
+
+                    cout << "Soy: " << getpid() << " tengo un poder de: " << poderAdam << " y tuve " << get<0>(simulacion) << " hijo/s" << endl;
+                
+				}
+                else{
+
+                    cout << "Soy: " << getpid() << " Mi papa es: " << getppid() << " tengo un poder de: " << poderHijo << " y tuve " << get<0>(simulacion) << " hijo/s" << endl;
+                
+				}                 
+            
+			}
+            else{
+
+                poderHijo = get<3>(simulacion);
+
+                if (diaActual == dias){
+
+                    cout << "El hijo: " << getpid() << " siendo su padre: " << getppid() << " tiene un poder de: " << poderHijo << " y engendro " << "0 hijos" << endl;
+                
+				}
+
+            }
+        }
+    }
+    if (getpid() != ppid){
+        exit(0);
+    }
+}
+
+void evitas(int dias, int m, float p1, float p3, atomic<float> *qtyEvas, atomic<float> *evitasPoder){
+
+	// recibe dias, y la probabilidad de hacer evitas los intenteos y el poder generado de este
+    qtyEvas[0] = 1;
+    float poderHijo = 0;
+    int ppid = getpid();
+    string type = "Evitas";
+    float poderlilith = Powers(type);
+    evitasPoder[0] = poderlilith;
+
+    for (int diaActual = 1; diaActual <= dias; diaActual++){
+        if(type == "Evitas"){
+           
+		    tuple<int, float, string, float> simulacion = spawnChildren(m, p1, p3, type);
+
+            if ("parent" == get<2>(simulacion)){
+
+                type = get<2>(simulacion);
+                qtyEvas[diaActual] = qtyEvas[diaActual] + get<0>(simulacion);
+                evitasPoder[diaActual] = evitasPoder[diaActual] + get<1>(simulacion);
+
+                if (ppid == getpid()){
+                   
+				    cout << "Soy: " << getpid() << " tiene un poder de: " << poderlilith << " y dio nacimiento a " << get<0>(simulacion) << " hijo/s" << endl;
+                
+				}
+                else{
+                
+				    cout << "Soy: " << getpid() << " Mi padre es:  " << getppid() << " tiene un poder de: " << poderHijo << " y dio nacimiento a " << get<0>(simulacion) << " hijo/s" << endl;
+                
+				}                 
+            }
+            else{
+
+                poderHijo = get<3>(simulacion);
+
+                if (diaActual == dias || get<2>(simulacion) == "invalid"){
+
+                    type = get<2>(simulacion);
+                    cout << "Soy: " << getpid() << " Mi padre es: " << getppid() << " tiene un poder de: " << poderHijo << " y dio nacimiento a " << "0 hijo/s" << endl;
+                
+				}
+
+            }
+        }
+    }
+    if (getpid() != ppid){
+        
+		exit(0);
+    
+	}
+}
+
+tuple<int, float, string, float> spawnChildren(int intentosNacimiento, float birthProb, float rageProb, string unitType){	
+
+	// toma el maximo intento de hijos, las probabilidades de nacimiento y la raza y hace la prueba de nacumiento 
+    int pid = -1;
+    int children = 0;
+    atomic<float> *poder = (atomic<float> *) share_mem(1);
+    float unitPower = 0;
+
+    srand(time(NULL));
+
+    for (int i = 0; i < intentosNacimiento; i++){
+        if (pid != 0){
+            if (((float)rand()) / ((float)RAND_MAX) * (0 - 100) + 100 < birthProb){
+                
+                children += 1;
+                pid = fork();
+            
+            }
+        }
+        if (pid == 0){
+
+            unitPower = Powers(unitType);
+
+            if (((float)rand()) / ((float)RAND_MAX) * (0 - 100) + 100 < rageProb){
+                
+                unitPower *= 2;
+                poder[0] = poder[0] + unitPower;
+                unitType = "invalid";
+                children = 0;
+            
+            }
+            else{
+             
+                poder[0] = poder[0] + unitPower;
+            
+            }
+
+            break;
+        }
+    }
+
+    float poderTotal = poder[0];
+
+    if (pid != 0){
+        for (int i = 0; i < children; i++){
+            
+            wait(NULL);
+        
+        }
+        
+        unitType = "parent";
+    
+    }
+
+    return make_tuple(children, poderTotal, unitType, unitPower);
+}
 
 void* share_mem(int size)
 {
-	// Vamos a pedir size * sizeof(atomic<int>) para reservar suficiendte memoria 
+	// Vamos a pedir size * sizeof(int) para reservar suficiendte memoria 
 	// para un vector sizeof nos dice el tamaño del tipo atomic int.
     void * mem;
     if( MAP_FAILED == (mem = (atomic<float>*)mmap(NULL, sizeof(atomic<float>)*size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0) ) )
@@ -147,15 +282,7 @@ void* share_mem(int size)
     return mem;
 }
 
-
-tuple<string, int> spawn_children(int max_days,int day, atomic<float> *newborns,
-		 						  float p, float p2, int max_children, 
-		 						  string my_type)
-{
-	//
-	int nchildren = 0;
-	int pid = -1;
-
+float Powers(string my_type){
 	// Seteamos los valores de la distribución según cada especie
 	float u = (my_type == "Evitas") ? 20 : 25;
 	float std = (my_type == "Evitas") ? 5 : 10;
@@ -163,28 +290,21 @@ tuple<string, int> spawn_children(int max_days,int day, atomic<float> *newborns,
 	// Inicializamos un generador con distribucion uniforme en el intervalo (0,1)
 	// La semilla es aleatoria, pero puede ser util fijarla para facilitar las pruebas	
 	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-	default_random_engine generator (3);
+    //semilla aca
+	default_random_engine generator (seed);  
  	uniform_real_distribution<double> distribution(0.0,1.0);
 
  	// Inicializamos un generador con distribucion normal
  	normal_distribution<double> ap_distribution(u,std);
 
-	for(int i = 0; i < max_children; i++)
-	{
-		// Simulamos un experimento de nacimiento. 
-		// Ej: Si p = 0.2  y el valor obtenido es <= a 0.2, el evita actual
-		// tiene exito en crear un nuevo hijo
-		bool isBorn =  true;
+    float poder = ap_distribution(generator);
 
-		// Si hay nacimiento tenemos que crear al evita/angel para que luego
-		// en day+1 continue con la simulación
-		if(isBorn)
-		{
-			int children = fork();
-		}
-	}
-	if (pid == -1 || pid > 0)
-		my_type = "parent"; 
-	
-	return make_tuple(my_type, nchildren);
+    if (poder < 0){
+
+        poder = 0;
+ 
+    }
+
+    return poder;
 }
+
